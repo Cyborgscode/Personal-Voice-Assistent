90903374dd751d72550621790c526c0c37f1b8bf
Cyborgscode <Cyborgscode@users.noreply.github.com>
2022-01-26 11:20:23 +0100

    Add files via upload

---
+++ shutdown.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+GREETING=$(grep "^conf:.*shutdown" /etc/pva/conf.d/* $HOME/.config/pva/conf.d/* | tail -n 1 | sed -e "s/^.*,//g" -e "s/\"//g")
+if [ "$GREETING" != "" ]; then
+        SAY=$(grep say $HOME/.config/pva/pva.conf| sed -e "s/^.*,//g" -e "s/\"//g" -e "s/%VOICE/$MLANG/g" -e "s/x:x/ /g")
+
+        if [ "$SAY" == "" ]; then
+                SAY="/usr/local/sbin/say"
+        fi
+        $SAY "$GREETING"
+fi
+
+
+PID=$(/usr/bin/pgrep -f ./pva.py)
+
+kill -9 $PID
+
---
+++ timer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /usr/share/pva
+
+java PVA
