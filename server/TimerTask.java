59e55a540741e0e570c35c7214daf91738235ffb
Cyborgscode <Cyborgscode@users.noreply.github.com>
2022-07-30 16:27:07 +0200

    switched to live imapchecks

--- server/TimerTask.java
+++ server/TimerTask.java
@@ -40,58 +40,6 @@ class TimerTask extends Thread {
 				if (isInterrupted()) {
 					return;
 				}
-
-				if ( pva.mailboxes.size() > 0 ) {
-					// don't load, if not needed
-					String mailtext = "";
-					MailConnection mc = new MailConnection();
-					for(int i=0; i < pva.mailboxes.size(); i++)	{
-						MailboxData m = pva.mailboxes.get(i);
-						mailtext += mc.checkmail(m);
-					}
-					
-					boolean playing = true;
-
-					// check if mediaplayers are running, to avoid interruption
-					
-					if ( !pva.config.get("mediaplayer","status").isEmpty() ) {
-
-						String allplayerservices = dos.readPipe( pva.config.get("mediaplayer","find").replaceAll( pva.config.get("conf","splitter")," ") );
-						if ( ! allplayerservices.isEmpty() ) {
-							String[] lines = allplayerservices.split("\n");
-							for(String service : lines ) {
-								
-								if ( service.contains("org.mpris.MediaPlayer2") ) {
-									
-									String 	vplayer = dos.readPipe( pva.config.get("mediaplayer","status")
-												      .replaceAll("<TARGET>", 
-													Tools.zwischen( service, "\"","\"") ).replaceAll( pva.config.get("conf","splitter")," ") );
-
-									if ( ! vplayer.trim().isEmpty() && vplayer.trim().contains("Playing") ) {
-										playing = false; // because a player is running.
-									}
-								}
-							}
-						}
-					}
-					
-					if ( ! dos.readPipe( "pgrep -f "+ pva.config.get("audioplayer","pname").replaceAll( pva.config.get("conf","splitter")," ") ).trim().isEmpty()  && !pva.config.get("audioplayer","status").isEmpty() ) {
-	
-						String[] result = dos.readPipe( pva.config.get("audioplayer","status").replaceAll( pva.config.get("conf","splitter")," "),true).split("\n");
-						for(String x : result ) {
-							if ( x.contains("[paused]") ) break;
-							if ( x.contains("TITLE") ) {
-								playing = false; // because a player is running.
-							}
-						}
-					}
-					
-					// voice notification should be played only if: no mediaplayer has status play OR DND mode shall be interrupted.
-					
-					if ( !mailtext.isEmpty() && ( playing || pva.config.get("conf","donotdisturb").equals("overwrite") ) ) {
-						pva.say(mailtext,true);
-					}
-				}
 	
 				long now = new Date().getTime();	
 				boolean update = false;			
@@ -120,8 +68,3 @@ class TimerTask extends Thread {
 		}
 	}
 }
-
-
-	
-
-	
