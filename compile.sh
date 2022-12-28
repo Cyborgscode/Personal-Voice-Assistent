1ccbd6d067f6a45df24d376697a267ad4256eeca
Cyborgscode <Cyborgscode@users.noreply.github.com>
2022-12-28 14:47:28 +0100

    we need to compile deeper in the tree ;)

--- compile.sh
+++ compile.sh
@@ -5,5 +5,6 @@ cd /usr/share/pva
 CP=""
 for file in lib/*;do  CP="$CP:./$file"; done 
 
-javac --release 8 server/PVA.java  */*.java -cp "$CP:."
+javac --release 8 server/PVA.java */*.java */*/*java -cp "$CP:."
+
 
