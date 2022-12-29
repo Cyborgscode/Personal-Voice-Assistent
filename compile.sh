ba452fda740dd2dba1776a1b9d735f0220b1fa90
Cyborgscode <Cyborgscode@users.noreply.github.com>
2022-12-29 13:54:28 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,6 +5,7 @@ cd /usr/share/pva
 CP=""
 for file in lib/*;do  CP="$CP:./$file"; done 
 
-javac --release 8 server/PVA.java */*.java */*/*java -cp "$CP:."
+javac --release 11 server/PVA.java */*.java */*/*java -cp "$CP:."
 
+# if you need Java 8 (legacy) change 11 back to 8
 
