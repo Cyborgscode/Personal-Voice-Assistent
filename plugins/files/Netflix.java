8cf49d8fdc6ff8cbd56cee45a9b404f5700c25a3
Cyborgscode <Cyborgscode@users.noreply.github.com>
2023-03-09 02:59:28 +0100

    NetFlix Controll *BETA*

--- plugins/files/Netflix.java
+++ plugins/files/Netflix.java
@@ -49,7 +49,27 @@ public class Netflix extends Plugin {
 		return true;
 	}
 	
+	private void activateNetflixWindow() {
+	
+		try {
+	
+			// activate Netflix browser window and be sure to be on top
+			pva.exec( pva.config.get("netflix","windowactivate").split(pva.config.get("conf","splitter")), wait);
 
+			// anyone with a better idea is welcome to share it...
+		
+			pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
+			pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
+			pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
+			pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
+			pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
+		} catch (Exception e) {
+			log(e.toString());
+			e.printStackTrace();
+		}
+	}
+
+	
 	final boolean wait = true;
 	
 	public String[] getActionCodes() {  return "NETFLIXHOME:NETFLIXMYLIST:NETFLIXSEARCH:NETFLIXRETURN:NETFLIXPAUSE:NETFLIXFORWARD:NETFLIXBACKWARDS:NETFLIXFULLSCREEN".split(":"); };
@@ -61,9 +81,7 @@ public class Netflix extends Plugin {
 
 					// FS mode can be only valid if we are in playing mode.
 
-					// activate Netflix browser window
-					pva.exec( pva.config.get("netflix","windowactivate").split(pva.config.get("conf","splitter")), wait);
-					
+					activateNetflixWindow();
 					
 					if ( vars.get("fullscreen").equals("no") ) {
 						pos.parse( "pos_fullscreen_on");
@@ -82,45 +100,147 @@ public class Netflix extends Plugin {
 				
 				if ( vars.get("playing").equals("no") ) {
 				
-					// activate Netflix browser window and be sure to be on top
-					pva.exec( pva.config.get("netflix","windowactivate").split(pva.config.get("conf","splitter")), wait);
-					pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
-					pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
-					pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
-					pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
-					pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
-					
+					activateNetflixWindow();
+					 
 					// now execute the real sequence
 				
 					pos.parse( "pos_mylist");
 					pva.exec( pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).split(pva.config.get("conf","splitter")), wait);
 					pva.exec( pva.config.get("netflix","clickLMB").split(pva.config.get("conf","splitter")), wait);
 
-					log( "DEBUG: "+ pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).replaceAll(pva.config.get("conf","splitter")," ") );
-					log( "DEBUG: "+ pva.config.get("netflix","clickLMB").replaceAll(pva.config.get("conf","splitter")," ") );
 				}										
 
 			} else if ( actioncode.equals("NETFLIXHOME") ) {
 				log("Netflix: activate HOME");
 				
 				if ( vars.get("playing").equals("no") ) {
-				
-					// activate Netflix browser window and be sure to be on top
-					pva.exec( pva.config.get("netflix","windowactivate").split(pva.config.get("conf","splitter")), wait);
-					pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
-					pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
-					pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
-					pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
-					pva.exec( pva.config.get("netflix","pageup").split(pva.config.get("conf","splitter")), wait);
+
+					activateNetflixWindow();
 					
 					// now execute the real sequence
 				
 					pos.parse( "pos_start");
 					pva.exec( pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).split(pva.config.get("conf","splitter")), wait);
 					pva.exec( pva.config.get("netflix","clickLMB").split(pva.config.get("conf","splitter")), wait);
+				}										
 
-					log( "DEBUG: "+ pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).replaceAll(pva.config.get("conf","splitter")," ") );
-					log( "DEBUG: "+ pva.config.get("netflix","clickLMB").replaceAll(pva.config.get("conf","splitter")," ") );
+			} else if ( actioncode.equals("NETFLIXRETURN") ) {
+				log("Netflix: activate RETURN");
+				
+				if ( vars.get("playing").equals("yes") ) {
+
+					activateNetflixWindow();
+					
+					// now execute the real sequence
+				
+					pos.parse( "pos_back");
+					pva.exec( pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).split(pva.config.get("conf","splitter")), wait);
+					pva.exec( pva.config.get("netflix","clickLMB").split(pva.config.get("conf","splitter")), wait);
+					vars.put("paused","no");
+					vars.put("playing","no");
+
+				}										
+
+			} else if ( actioncode.equals("NETFLIXPAUSE") ) {
+				log("Netflix: activate PAUSE/PLAY");
+
+				if ( vars.get("playing").equals("yes") ) {
+
+					activateNetflixWindow();
+					
+					// now execute the real sequence
+				
+					pos.parse( "pos_play");
+					pva.exec( pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).split(pva.config.get("conf","splitter")), wait);
+					pva.exec( pva.config.get("netflix","clickLMB").split(pva.config.get("conf","splitter")), wait);
+
+					if ( vars.get("paused").equals("no") ) {
+						vars.put("paused","yes");
+					} else {
+						vars.put("paused","no");
+					}
+
+				}										
+
+			} else if ( actioncode.equals("NETFLIXFORWARD") ) {
+				log("Netflix: activate FORWARD");
+				
+				if ( vars.get("playing").equals("yes") ) {
+
+					activateNetflixWindow();
+					
+					// now execute the real sequence
+				
+					pos.parse( "pos_forward");
+					pva.exec( pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).split(pva.config.get("conf","splitter")), wait);
+					pva.exec( pva.config.get("netflix","clickLMB").split(pva.config.get("conf","splitter")), wait);
+
+				}										
+
+			} else if ( actioncode.equals("NETFLIXBACKWARDS") ) {
+				log("Netflix: activate BACKWARDS");
+				
+				if ( vars.get("playing").equals("yes") ) {
+
+					activateNetflixWindow();
+					
+					// now execute the real sequence
+				
+					pos.parse( "pos_backwards");
+					pva.exec( pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).split(pva.config.get("conf","splitter")), wait);
+					pva.exec( pva.config.get("netflix","clickLMB").split(pva.config.get("conf","splitter")), wait);
+
+				}										
+
+			} else if ( actioncode.equals("NETFLIXSEARCH") ) {
+				log("Netflix: activate SEARCH");
+
+				if ( vars.get("playing").equals("yes") ) {
+					activateNetflixWindow();
+					pos.parse( "pos_back");
+					pva.exec( pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).split(pva.config.get("conf","splitter")), wait);
+					pva.exec( pva.config.get("netflix","clickLMB").split(pva.config.get("conf","splitter")), wait);
+					Thread.sleep(2000L);
+					vars.put("playing","no");
+					vars.put("paused","no");
+				}
+				
+				if ( vars.get("playing").equals("no") ) {
+				
+					String searchterm = rawtext.trim();
+
+					if ( pva.config.get("conf","lang_short").equals("de") ) {
+						// if we run i.e. german Y & Z need to be exchanged as xdotool does not honor $LANG :/
+						searchterm = searchterm.replaceAll("y",":&:").replaceAll("z","y").replaceAll(":&:","z");
+					}
+
+					log("search "+ searchterm);
+
+					activateNetflixWindow();
+
+					// now execute the real sequence
+					pva.exec( pva.config.get("netflix","escape").split(pva.config.get("conf","splitter")), wait);
+					pos.parse( "pos_start");
+					pva.exec( pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).split(pva.config.get("conf","splitter")), wait);
+					pva.exec( pva.config.get("netflix","clickLMB").split(pva.config.get("conf","splitter")), wait);
+					Thread.sleep(1000L);
+					pos.parse( "pos_search");
+					pva.exec( pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).split(pva.config.get("conf","splitter")), wait);
+					pva.exec( pva.config.get("netflix","clickLMB").split(pva.config.get("conf","splitter")), wait);
+					Thread.sleep(1000L);
+					pva.exec( pva.config.get("netflix","type").replaceAll("<TERM1>", searchterm+"\n") .split(pva.config.get("conf","splitter")), wait);
+					pva.exec( pva.config.get("netflix","linefeed").split(pva.config.get("conf","splitter")), wait);
+					pos.parse( "pos_firstentry");
+					pva.exec( pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).split(pva.config.get("conf","splitter")), wait);
+					pva.exec( pva.config.get("netflix","clickLMB").split(pva.config.get("conf","splitter")), wait);
+					Thread.sleep(2000L);
+					pos.parse( "pos_miniplay");
+					pva.exec( pva.config.get("netflix","mousemove").replaceAll("XXX", ""+pos.x ).replaceAll("YYY", ""+pos.y ).split(pva.config.get("conf","splitter")), wait);
+					pva.exec( pva.config.get("netflix","clickLMB").split(pva.config.get("conf","splitter")), wait);
+
+					vars.put("playing","yes");
+
+					
 				}										
 
 			} else return false;
@@ -167,5 +287,4 @@ class Position {
 	}
 		
 }
-
 
