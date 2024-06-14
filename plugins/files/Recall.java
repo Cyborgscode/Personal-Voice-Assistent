cee2d0990c8879e69707175c66b93c88ebec96dd
Cyborgscode <Cyborgscode@users.noreply.github.com>
2024-06-14 14:52:32 +0200

    Update to newest recall config system

--- plugins/files/Recall.java
+++ plugins/files/Recall.java
@@ -75,7 +75,7 @@ public class Recall extends Plugin {
 					return true;
 				}
 
-				String pathname = dos.readFile( pva.getHome()+"/.config/recall/path" ).trim();
+				String pathname = dos.readFile( pva.getHome()+"/.config/recall/config" ).trim();
 				if ( !pathname.endsWith("/") ) {
 					pathname += "/";
 				}
