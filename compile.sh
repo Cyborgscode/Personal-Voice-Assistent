d7dd6936d519b401f65174a5821c21cec4579df1
Cyborgscode <Cyborgscode@users.noreply.github.com>
2025-11-13 10:25:01 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 cd /usr/share/pva
 
 CP=""
-for file in lib/*;do  CP="$CP:./$file"; done 
+for file in lib/*jar;do  CP="$CP:./$file"; done 
 
 javac --release 11 server/PVA.java */*.java */*/*java -cp "$CP:."
 
