1df040fb87abe6d405ae42dc7416781cd6aaa7c3
Cyborgscode <Cyborgscode@users.noreply.github.com>
2026-02-13 19:30:55 +0100

    pva.java removed

--- compile.sh
+++ compile.sh
@@ -8,7 +8,7 @@ cd /usr/share/pva
 CP=""
 for file in lib/*jar;do  CP="$CP:./$file"; done 
 
-javac --release 11 pva.java server/PVA.java */*.java */*/*java -cp "$CP:."
+javac --release 11 server/PVA.java */*.java */*/*java -cp "$CP:."
 
 # if you need Java 8 (legacy) change 11 back to 8
 
