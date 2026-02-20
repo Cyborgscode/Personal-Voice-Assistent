f568ab825fe4fc445f1ec4ee47f395372c5d0581
Cyborgscode <Cyborgscode@users.noreply.github.com>
2026-02-20 15:02:20 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -19,8 +19,9 @@ fi
 # We use our own HTTP Class, which got invented years before java implemented theire own, so we need to focus on our own class files, in case you wonder why this looks unnecessary. It isn't ;)
 # It compiles fines with Java 21. The "--release 11" is just backwards compatibility for older javas. You are free to remove it if you wanne a newer Java
 
-
 for file in lib/*jar;do  CP="$CP:./$file"; done 
+
+echo "Classpath: $CP"
 
 javac --release 11 server/PVA.java */*.java */*/*java -cp "$CP:."
 
