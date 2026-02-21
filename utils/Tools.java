71adf756ef4ccd52c41b8689c43384d9fee51d6d
Cyborgscode <Cyborgscode@users.noreply.github.com>
2026-02-21 21:53:19 +0100

    Update Tools.java

--- utils/Tools.java
+++ utils/Tools.java
@@ -1,4 +1,3 @@
-
 package utils;
 
 public class Tools {
@@ -16,5 +15,56 @@ public class Tools {
 
                 return null;
         }
+
+	// JSON Object is given by LLM, but the org.json package can't be shipped with distros, so we need to do it ourself, so .. don't wonder it's messy ;)
+
+	public static String parseJSON(String json) {
+	
+		json=json.replaceAll("\\\\.","");
+		
+		String[] pairs = json.split("(\",\"|},\")");
+					
+		String answere = "";
+					
+		for(String pair: pairs) {
+//			log( "pair = "+ pair);
+										
+			if ( pair.contains(":") ) {
+				String[] data = pair.split(":",2);
+				if ( data.length > 1 ) {	
+
+					String key = data[0].replaceAll("\"","");
+					String value = data[1];
+												
+//					log("key="+ key +"\nvalue="+ value );
+
+					if ( key.endsWith("\"") ) key = key.substring(0,key.indexOf("\"")-1);
+					if ( value.endsWith("\"") ) value = value.substring(0,value.indexOf("\"",1));
+
+//					log("key="+ key +"\nvalue="+ value );
+												
+					if ( ( key.equals("response") || key.equals("content") ) && value.trim().length() > 1 ) {
+						answere = value.substring(1).replaceAll("\\n","\n");
+					}
+				}
+				if ( answere == null ) answere = "";
+			} 
+		}
+
+		return answere;
+		
+	}
+
+	static public String filterAIThinking(String answere) {
+	
+		if ( answere.contains("003cthink003e") ) {
+			int abis = answere.indexOf("003c/think003e")+"003c/think003e".length();
+			if ( abis >= 0 ) 
+				answere = answere.substring( abis );
+		}
+		return answere.replaceAll("\\*\\*","").replaceAll("\\*","\"");
+	}
+
+
 
 }
