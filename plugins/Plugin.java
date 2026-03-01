4a32558066fa5b3353bd0cfa50db3b1845c7c744
Cyborgscode <Cyborgscode@users.noreply.github.com>
2026-03-01 22:10:48 +0100

    TNG - IntentSuperSystem

--- plugins/Plugin.java
+++ plugins/Plugin.java
@@ -10,14 +10,20 @@ import data.Command;
 abstract public class Plugin extends Thread implements PluginInterface {
 
 	protected PVA pva;
-
 	protected Dos dos = new Dos();
+	
+	protected void say(String text,String returnIntent,String extra) {
+		String sender = getPluginInfo().get("name");
+		if ( sender.isEmpty() ) sender="UNKOWN";
+		log(sender+":say():"+ returnIntent+":"+extra);
+		pva.AsyncSendIntent(new Command( sender , "ASYNCSPEAK", returnIntent, extra), text );
+	}
 	protected void say(String text) {
 		String sender = getPluginInfo().get("name");
 		if ( sender.isEmpty() ) sender="UNKOWN";
+		log(sender+":say()");
 		pva.AsyncSendIntent(new Command( sender , "ASYNCSPEAK", "", ""), text );
 	}
-
 	protected String getT(String key) {
 		return pva.getText(key);
 	}
