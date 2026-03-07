423ceca5632f86a29d6a7245122e959121f69416
Cyborgscode <Cyborgscode@users.noreply.github.com>
2026-03-07 15:26:16 +0100

    added opencv support

--- start.sh
+++ start.sh
@@ -5,4 +5,13 @@ cd /usr/share/pva
 CP=""
 for file in lib/*;do  CP="$CP:./$file"; done 
 for file in lib/*/*;do  CP="$CP:./$file"; done
-java -cp "$CP:." server.PVA "startserver"
+ADD=""
+
+if [ -e plugins/files/WatchdogPlugin.java ]; then
+
+	CP="/usr/lib/java/opencv.jar:$CP"
+	ADD="-Djava.library.path=/usr/lib/java/"
+	LD_LIBRARY_PATH=/usr/lib/java:/usr/lib64:$LD_LIBRARY_PATH
+fi
+
+java  $ADD -cp "$CP:." server.PVA "startserver"  >> $HOME/.var/log/pva.log 2>> $HOME/.var/log/pva.log
