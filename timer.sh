4d83163e8dce6d839fac027ef542fda1aeb959a2
Cyborgscode <Cyborgscode@users.noreply.github.com>
2026-03-09 09:11:25 +0100

    obsoleted in 2023

--- timer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script is called ponce per minute by the systemd.timer, we create on first launch of PVA.
-# it will just load the timer db and check, if a reminder should be presented (accustically)
-
-cd /usr/share/pva
-CP=""
-for file in lib/*;do  CP="$CP:./$file"; done 
-java -cp "$CP:." PVA
